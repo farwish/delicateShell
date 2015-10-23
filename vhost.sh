91b7b375335dc61baf22be64323c174a15d8e6f9
weichen <farwish@live.com>
2015-10-23 13:37:11 +0800

    vhost v1.0:修正规范

--- vhost.sh
+++ vhost.sh
@@ -12,8 +12,8 @@ echo "
 # 输入检测
 read -p "请输入Nginx所安装目录(如:/usr/local/nginx):" -t 30 nginxdir
 
-	# 注意不正确的语法: [ -z $nginxdir || ! -d $nginxdir ]
-	if [ -z $nginxdir ] || [ ! -d $nginxdir ]
+	# 注意不正确的语法: [ -z "$nginxdir" || ! -d "$nginxdir" ]
+	if [ -z "$nginxdir" ] || [ ! -d "$nginxdir" ]
 		then
 			echo "Error:输入的不是目录"
 			exit 1
@@ -21,7 +21,7 @@ read -p "请输入Nginx所安装目录(如:/usr/local/nginx):" -t 30 nginxdir
 													 
 read -p "请输入Nginx虚拟主机地址(如:abc.example.com):" -t 30 vhostaddr
 
-	if test -z $vhostaddr; then
+	if test -z "$vhostaddr"; then
 		echo "Error:输入不正确"
 		exit 2
 	fi
@@ -29,20 +29,20 @@ read -p "请输入Nginx虚拟主机地址(如:abc.example.com):" -t 30 vhostaddr
 read -p "请输入Nginx虚拟主机端口(如:80):" -t 30 vhostport
 
 	# sed 's/^[0-9]*$//g' 如果替换之后为空,则是数字
-	port=$(echo $vhostport | sed 's/^[0-9]*$//g')
-	if [ ! -z $port ]; then
+	port=$(echo "$vhostport" | sed 's/^[0-9]*$//g')
+	if [ ! -z "$port" ]; then
 		echo "Error:输入的不是数字"
 		exit 3
 	fi
 
-	if [ ! $vhostport -gt 0 ]; then
+	if [ ! "$vhostport" -gt 0 ]; then
 		echo "Error:输入的数字不正确"
 		exit 4
 	fi
 
 read -p "请输入Nginx虚拟主机访问目录(如:/home/www/abc):" -t 30 wwwdir
 
-	if [ -z $wwwdir ]; then
+	if [ -z "$wwwdir" ]; then
 		echo "Error:没有输入目录";
 		exit 5
 	fi
@@ -61,35 +61,36 @@ read -p "请确认您的输入(y/n):" -t 30 enter
 
 confaddr=$nginxdir"/conf/vhost/"$vhostaddr".conf"
 
-confitem="
-server {
-    listen       "$vhostport";
-    server_name  "$vhostaddr";
-
-    location / {
-        index  index.html index.htm index.php;
-        root   "$vhostdir";
-        if (!-e $request_filename){
-            rewrite ^(.*)$ /index.php/?s=$1 last;
-            break;
-        }
-    }
-
-    location ~ \.php {
-        root           "$vhostdir";
-        fastcgi_pass   127.0.0.1:9000;
-        fastcgi_index  index.php;
-        fastcgi_split_path_info ^(.+\.php)(.*)$;
-        fastcgi_param PATH_INFO $fastcgi_path_info;
-        fastcgi_param SCRIPT_FILENAME  $document_root$fastcgi_script_name;
-        include        fastcgi_params;
-    }
-}"
+confitem() {
+echo "	server {"
+echo "		listen       "$vhostport";"
+echo "		server_name  "$vhostaddr";"
+echo ""
+echo "		location / {"
+echo "			index  index.html index.htm index.php;"
+echo "			root   "$wwwdir";"
+echo "			if (!-e $request_filename){"
+echo "				#rewrite ^(.*)$ /index.php/?s=$1 last;"
+echo "				break;"
+echo "			}"
+echo "		}"
+echo ""
+echo "		location ~ \.php {"
+echo "			root           "$wwwdir";"
+echo "			fastcgi_pass   127.0.0.1:9000;"
+echo "			fastcgi_index  index.php;"
+echo "			fastcgi_split_path_info ^(.+\.php)(.*)$;"
+echo "			fastcgi_param PATH_INFO $fastcgi_path_info;"
+echo "			fastcgi_param SCRIPT_FILENAME  $document_root$fastcgi_script_name;"
+echo "			include        fastcgi_params;"
+echo "		}"
+echo "	}"
+}
 
 if [ "$enter" == "y" ]
 	then
 		# 检测是否已有目录
-		if [ -d $wwwdir ]; then
+		if [ -d "$wwwdir" ]; then
 			read -p "[注意: 目录 "$wwwdir" 已存在] 是否覆盖(y/n):" -t 30 entertwo
 			
 			if test "$entertwo" == "n"
@@ -97,16 +98,16 @@ if [ "$enter" == "y" ]
 					read -p "请重新输入虚拟主机访问目录:" -t 30 wwwdir
 				else
 					echo "...删除目录"$wwwdir
-					rm -rf $wwwdir
+					rm -rf "$wwwdir"
 			fi
 		fi
 
 		# 新建主机访问目录
 		echo "...创建目录"$wwwdir
-		mkdir $wwwdir
+		mkdir "$wwwdir"
 
 		# 检测配置文件是否已存在
-		if [ -e $confaddr ]; then
+		if [ -e "$confaddr" ]; then
 			read -p "[注意: 配置文件 "$confaddr" 已存在] 是否覆盖(y/n):" -t 30 enterthree
 
 			if test "$enterthree" == "n"
@@ -117,16 +118,16 @@ if [ "$enter" == "y" ]
 					confaddr=$nginxdir"/conf/vhost/"$confaddr
 				else
 					echo "...删除配置文件"$confaddr
-					rm -rf $confaddr
+					rm -rf "$confaddr"
 			fi
 		fi
 
 		echo "...新建配置文件"$confaddr
-		touch $confaddr
+		touch "$confaddr"
 
-		echo $confitem > $confaddr
+		confitem > "$confaddr"
 
-		echo "完成!"
+		echo "...完成!"
 	else
 		exit 6
 fi
