9193b3a5aba1201e4fb964d576eac1d379c3d417
weiChen <farwish@live.com>
2016-11-15 16:44:52 +0800

    Update installNginx.sh

--- lnmp/installNginx.sh
+++ lnmp/installNginx.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
-# Nginx安装
+# 源码安装Nginx
 # @farwish.com BSD-License
 
 archiveDir=/opt/Archive/
-
-nginx_path=/usr/local/nginx/
-
+nginx_source_url=http://nginx.org/download/nginx-1.10.1.tar.gz
+nginx_install_dir=/usr/local/nginx/
 nginx_bagname=nginx-1.10.1.tar.gz
-
 nginx_src=nginx-1.10.1
 
-if [ ! -e ${nginx_bagname} ]; then
-	wget http://nginx.org/download/nginx-1.10.1.tar.gz
-	tar zxf ${nginx_bagname}
+if [ ! -e ${archiveDir}${nginx_bagname} ]; then
+	wget ${nginx_source_url}
+	tar zxf ${archiveDir}${nginx_bagname}
 fi
 
-cd ${arch_path}${nginx_src}
+cd ${archiveDir}${nginx_src}
 
 echo "配置编译Nginx..."
 
-./configure --prefix=${nginx_path} --conf-path=${nginx_path}conf/nginx.conf
+./configure --prefix=${nginx_install_dir} --conf-path=${nginx_install_dir}conf/nginx.conf
 
 make && make install
 
 echo "设置环境变量..."
-echo -n ":/usr/local/nginx/sbin" >> /etc/profile
+echo -n ":${nginx_install_dir}sbin" >> /etc/profile
 
 echo "启动..."
 nginx
