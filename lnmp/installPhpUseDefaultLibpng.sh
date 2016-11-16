f8d668d187e0bccc79281c00e0910de282d374d6
weiChen <farwish@live.com>
2016-11-16 18:08:31 +0800

    success

--- lnmp/installPhpUseDefaultLibpng.sh
+++ lnmp/installPhpUseDefaultLibpng.sh
@@ -29,16 +29,20 @@ freetype_url=http://downloads.sourceforge.net/project/freetype/freetype2/2.7/${f
 yum install -y libxml libxml2 libxml2-devel libpng libpng-devel libjpeg libjpeg-devel freetype freetype-devel curl curl-devel openssl openssl-devel zlib-devel
 
 # centos7 kernel-3.10.237中已不含libmcrypt, 需自行下载
-wget http://sourceforge.net/projects/mcrypt/files/Libmcrypt/2.5.8/libmcrypt-2.5.8.tar.gz
-tar zxf libmcrypt-2.5.8.tar.gz
+if [ ! -d libmcrypt-2.5.8 ]; then
+        wget http://sourceforge.net/projects/mcrypt/files/Libmcrypt/2.5.8/libmcrypt-2.5.8.tar.gz
+        tar zxf libmcrypt-2.5.8.tar.gz
+fi
 cd libmcrypt-2.5.8
 ./configure --prefix=/usr/local/libmcrypt
 make && make install
 
 cd ${arch_path}
 
-echo "下载 "${freetype_bagname}" ... "
-wget ${freetype_url}
+if [ ! -e ${freetype_bagname} ]; then
+        echo "下载 "${freetype_bagname}" ... "
+        wget ${freetype_url}
+fi
 
 if [ -e ${freetype_bagname} ]; then
         echo "正在解压 "${freetype_bagname}"..."
@@ -56,9 +60,11 @@ make && make install
 
 cd ${arch_path}
 
-echo "下载PHP..."
-wget http://cn2.php.net/distributions/php-5.6.25.tar.gz
-tar zxf php-5.6.25.tar.gz
+if [ ! -d ${arch_path_php} ]; then
+        echo "下载PHP..."
+        wget http://cn2.php.net/distributions/php-5.6.25.tar.gz
+        tar zxf php-5.6.25.tar.gz
+fi
 
 echo "配置PHP..."
 
@@ -67,7 +73,7 @@ cd ${arch_path_php}
 # 要使用非pdo等其它驱动，推荐使用mysqlnd：http://php.net/manual/en/mysqlinfo.library.choosing.php
 # ./configure --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --with-mysql=mysqlnd
 # 注意：不使用上面下载的最新libpng、libjpeg，PHP不支持，就用yum安装的.
-./configure --with-libdir=lib64 --prefix=${php_path} --with-mysqli --with-pdo-mysql=/usr/local/mysql --enable-inline-optimization --enable-fpm --with-freetype-dir=/usr/local/freetype --with-gd --with-zlib --with-png-dir --with-jpeg-dir --enable-mbstring --with-iconv --enable-sockets --with-curl --with-mcrypt=/usr/local/libmcrypt --with-openssl --enable-pcntl --enable-soap --enable-opcache
+./configure --with-libdir=lib64 --prefix=${php_path} --with-mysqli --with-pdo-mysql=/usr/local/mysql --enable-inline-optimization --enable-fpm --with-freetype-dir=/usr/local/freetype --with-gd --with-zlib --with-png-dir --with-jpeg-dir --enable-mbstring --with-iconv --enable-sockets --with-curl --with-mcrypt-dir=/usr/local/libmcrypt --with-openssl --enable-pcntl --enable-soap --enable-opcache
 
 echo "编译PHP..."
 
