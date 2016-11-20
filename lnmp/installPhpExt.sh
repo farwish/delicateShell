de785ee256f2fac1ffb4bf17dd81ea8dd2f286cc
weiChen <farwish@live.com>
2016-11-20 23:47:10 +0800

    Update installPhpExt.sh

--- lnmp/installPhpExt.sh
+++ lnmp/installPhpExt.sh
@@ -1,8 +1,30 @@
 #/bin/bash
-# 一些php扩展安装
+# php扩展全家桶.
 # @farwish.com BSD-License
 
+php_path=/usr/local/php5.6.25/
+cur_dir=`pwd`
+yum install -y git
+
+# extension=eio.so
 pecl install eio
 
+# extension=libevent.so
 yum install -y libevent-devel.x86_64
 pecl install libevent-0.1.0
+
+# extension=redis.so
+if [ ! -d ${cur_dir}/phpredis ]; then
+    git clone -b 2.2.8 https://github.com/phpredis/phpredis.git
+fi
+cd phpredis
+phpize
+./configure --with-php-config=${php_path}bin/php-config
+make && make install
+
+# extension=swoole.so
+pecl install swoole
+
+echo -e "\nCompleted!\n"
+echo -e "\n需要在php.ini中加入的配置有:\n"
+echo -e " extension=eio.so\n extension=libevent.so\n extension=redis.so\n extension=swoole.so\n"
